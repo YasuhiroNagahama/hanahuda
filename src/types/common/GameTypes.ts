6c47439bb45e5034c65d6023056936f0aebfe1d3
YasuhiroNagahama <fnifhubi85h29bddi@gmail.com>
2024-02-06 13:56:21 +0900

    大文字に変更

--- src/types/common/GameTypes.ts
+++ src/types/common/GameTypes.ts
@@ -1,16 +1,16 @@
 enum GameTypes {
-  Koikoi = "koikoi",
-  Roppyakken = "roppyakken",
-  Hanaawase = "hanaawase",
-  Hatihati = "hatihati",
-  Hatihatiginmi = "hatihatiginmi",
-  Kingo = "kingo",
-  Musi = "musi",
-  Etyobo = "etyobo",
-  Otioikabu = "otioikabu",
-  Hikikabu = "hikikabu",
-  Poka = "poka",
-  None = "none",
+  Koikoi = "Koikoi",
+  Roppyakken = "Roppyakken",
+  Hanaawase = "Hanaawase",
+  Hatihati = "Hatihati",
+  Hatihatiginmi = "Hatihatiginmi",
+  Kingo = "Kingo",
+  Musi = "Musi",
+  Etyobo = "Etyobo",
+  Otioikabu = "Otioikabu",
+  Hikikabu = "Hikikabu",
+  Poka = "Poka",
+  None = "None",
 }
 
 export { GameTypes };
